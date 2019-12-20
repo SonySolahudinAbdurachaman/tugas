d712cf5ca244c523c6a5e2629c665cd33c7f2236
sonysolahudinabdurachman <sonymoonraker@gmail.com>
2019-12-20 22:40:35 +0700

    tugassss

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/*
+ * Nama : Sony Solahudin Abdurachman
+ * NIM  : A2.1900171
+ * Kelas: TI-1C
+ *
+ * @author SonySA
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
